d185bc161d0cbd941fcac7e0f5ecce8e919384f8
Wendell Patton Jr <wendell.pattonjr@gmail.com>
2021-02-04 20:59:32 -0600

    random square exercise complete

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RandomSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //using Random class generate  list of 20 random numbers between 1-50
+            Random random = new Random();
+            var listOfNumbers = new List<int>();
+            for (int i = 0; i < 20; i++)
+            {
+                listOfNumbers.Add(random.Next(1, 50));
+            }
+            static void Square(int number)
+            {
+                number = number * number;
+            }
+            listOfNumbers.ForEach(Square);
+            List<int> filteredOutNumbers = listOfNumbers.FindAll(number => number % 2 == 0);
+            filteredOutNumbers.ForEach(number => Console.WriteLine(number));
+        }
+    }
+}
